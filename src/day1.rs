18aa282d5980ce70776a999a706c5e41890e0d44
Michael Rybakov <me@opilar.com>
2019-12-26 18:48:32 +0300

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -7,11 +7,37 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve(input: &[u32]) -> u32 {
+pub fn part1(input: &[u32]) -> u32 {
     input
         .iter()
-        .map(|mass| {
-            mass/3 - 2
-        })
+        .copied()
+        .map(calc_fuel)
         .sum()
+}
+
+fn calc_fuel(mass: u32) -> u32 {
+    if mass <= 6 {
+        0
+    } else {
+        mass/3 - 2
+    }
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[u32]) -> u32 {
+    input
+        .iter()
+        .copied()
+        .map(calc_fuel_with_additional_fuel)
+        .sum()
+}
+
+fn calc_fuel_with_additional_fuel(mass: u32) -> u32 {
+    let mut fuel_needed = calc_fuel(mass);
+    let mut result = fuel_needed;
+    while fuel_needed != 0 {
+        fuel_needed = calc_fuel(fuel_needed);
+        result += fuel_needed;
+    }
+    result
 }
